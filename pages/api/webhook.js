6d9e3dd59d76790a078fb49782e5451a9947523d
Benjvelt <benjamin@velt.dev>
2023-08-31 18:21:33 -0700

    go

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -2,13 +2,191 @@
 
 export default function handler(req, res) {
 
-    let commentAnnotation = ""
+   
+    res.status(200).json({
+        "commentAnnotation": {
+          "annotationId": "-NdDJhxEEljnpY-n8gH6",
+          "annotationIndex": 37,
+          "color": "#a259fe",
+          "comments": [
+            {
+              "commentHtml": "test",
+              "commentId": 287394,
+              "commentText": "test",
+              "from": {
+                "color": "#FFD600",
+                "contacts": [
+                  {
+                    "email": "john.doe@snippyly.com",
+                    "name": "John Doe",
+                    "photoUrl": "https://i.pravatar.cc/300",
+                    "userId": "d5558f1f-bdea-4eb5-9fd5-ed657e460307",
+                    "visibility": "group"
+                  },
+                  {
+                    "email": "mary.doe@snippyly.com",
+                    "name": "Mary Doe",
+                    "photoUrl": "https://i.pravatar.cc/300",
+                    "userId": "d5558f1f-bdea-4eb5-9fd5-ed657e460308",
+                    "visibility": "group"
+                  }
+                ],
+                "email": "ben@velt.dev",
+                "groupId": "admins",
+                "name": "ben",
+                "photoUrl": "https://media.licdn.com/dms/image/C5603AQHgHt3GquPb0g/profile-displayphoto-shrink_400_400/0/1650605489047?e=1696464000&v=beta&t=9056kSSbnv5pNtD6IfTKJ6Facx5LV7myV2FwE5sEv7A",
+                "type": "signedIn",
+                "userId": "123ben",
+                "userSnippylyId": "4658641886513752"
+              },
+              "lastUpdated": "2023-09-01T01:19:29.969Z",
+              "status": "added",
+              "type": "text"
+            }
+          ],
+          "from": {
+            "color": "#FFD600",
+            "contacts": [
+              {
+                "email": "john.doe@snippyly.com",
+                "name": "John Doe",
+                "photoUrl": "https://i.pravatar.cc/300",
+                "userId": "d5558f1f-bdea-4eb5-9fd5-ed657e460307",
+                "visibility": "group"
+              },
+              {
+                "email": "mary.doe@snippyly.com",
+                "name": "Mary Doe",
+                "photoUrl": "https://i.pravatar.cc/300",
+                "userId": "d5558f1f-bdea-4eb5-9fd5-ed657e460308",
+                "visibility": "group"
+              }
+            ],
+            "email": "ben@velt.dev",
+            "groupId": "admins",
+            "name": "ben",
+            "photoUrl": "https://media.licdn.com/dms/image/C5603AQHgHt3GquPb0g/profile-displayphoto-shrink_400_400/0/1650605489047?e=1696464000&v=beta&t=9056kSSbnv5pNtD6IfTKJ6Facx5LV7myV2FwE5sEv7A",
+            "type": "signedIn",
+            "userId": "123ben",
+            "userSnippylyId": "4658641886513752"
+          },
+          "lastUpdated": 1693531168643,
+          "pageInfo": {
+            "commentUrl": "https://mixpanel-demo.vercel.app/?username=ben&scommentId=-NdDJhxEEljnpY-n8gH6",
+            "deviceInfo": {
+              "browserName": "Chrome",
+              "browserVersion": "116",
+              "deviceType": "Desktop",
+              "orientation": "landscape",
+              "osName": "Macintosh",
+              "osVersion": "10.15.7",
+              "screenHeight": 982,
+              "screenWidth": 1512,
+              "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+            }
+          },
+          "position": {
+            "left": 648,
+            "top": 0
+          },
+          "positionX": 720.5,
+          "positionY": -20,
+          "screenHeight": 2241,
+          "screenScrollHeight": 2241,
+          "screenWidth": 1441,
+          "status": {
+            "color": "#625DF5",
+            "id": "OPEN",
+            "lightColor": "#E7E8FA",
+            "name": "Open",
+            "svg": "\n      <svg\n        width=\"16\"\n        height=\"16\"\n        viewBox=\"0 0 16 16\"\n        fill=\"none\"\n        xmlns=\"http://www.w3.org/2000/svg\"\n      >\n        <path\n          fill-rule=\"evenodd\"\n          clip-rule=\"evenodd\"\n          d=\"M8.00016 13.3333C10.9457 13.3333 13.3335 10.9455 13.3335 8.00001C13.3335 5.05449 10.9457 2.66668 8.00016 2.66668C5.05464 2.66668 2.66683 5.05449 2.66683 8.00001C2.66683 10.9455 5.05464 13.3333 8.00016 13.3333ZM8.00016 14.6667C11.6821 14.6667 14.6668 11.6819 14.6668 8.00001C14.6668 4.31811 11.6821 1.33334 8.00016 1.33334C4.31826 1.33334 1.3335 4.31811 1.3335 8.00001C1.3335 11.6819 4.31826 14.6667 8.00016 14.6667Z\"\n          fill=\"currentColor\"\n        />\n      </svg>\n      ",
+            "type": "default"
+          },
+          "taggedElementPath": "",
+          "targetElement": {
+            "fXpath": "/html/body/div[1]/div/div/div[2]/div[2]",
+            "leftPercentage": 90,
+            "topPercentage": 50,
+            "xpath": "//*[@id='comment-2']"
+          },
+          "targetElementId": "comment-2",
+          "type": "comment"
+        },
+        "targetComment": {
+          "commentHtml": "test",
+          "commentId": 287394,
+          "commentText": "test",
+          "from": {
+            "color": "#FFD600",
+            "contacts": [
+              {
+                "email": "john.doe@snippyly.com",
+                "name": "John Doe",
+                "photoUrl": "https://i.pravatar.cc/300",
+                "userId": "d5558f1f-bdea-4eb5-9fd5-ed657e460307",
+                "visibility": "group"
+              },
+              {
+                "email": "mary.doe@snippyly.com",
+                "name": "Mary Doe",
+                "photoUrl": "https://i.pravatar.cc/300",
+                "userId": "d5558f1f-bdea-4eb5-9fd5-ed657e460308",
+                "visibility": "group"
+              }
+            ],
+            "email": "ben@velt.dev",
+            "groupId": "admins",
+            "name": "ben",
+            "photoUrl": "https://media.licdn.com/dms/image/C5603AQHgHt3GquPb0g/profile-displayphoto-shrink_400_400/0/1650605489047?e=1696464000&v=beta&t=9056kSSbnv5pNtD6IfTKJ6Facx5LV7myV2FwE5sEv7A",
+            "type": "signedIn",
+            "userId": "123ben",
+            "userSnippylyId": "4658641886513752"
+          },
+          "lastUpdated": "2023-09-01T01:19:29.969Z",
+          "status": "added",
+          "type": "text"
+        },
+        "actionType": "added",
+        "notificationSource": "comment",
+        "actionUser": {
+          "color": "#FFD600",
+          "contacts": [
+            {
+              "email": "john.doe@snippyly.com",
+              "name": "John Doe",
+              "photoUrl": "https://i.pravatar.cc/300",
+              "userId": "d5558f1f-bdea-4eb5-9fd5-ed657e460307",
+              "visibility": "group"
+            },
+            {
+              "email": "mary.doe@snippyly.com",
+              "name": "Mary Doe",
+              "photoUrl": "https://i.pravatar.cc/300",
+              "userId": "d5558f1f-bdea-4eb5-9fd5-ed657e460308",
+              "visibility": "group"
+            }
+          ],
+          "email": "ben@velt.dev",
+          "groupId": "admins",
+          "name": "ben",
+          "photoUrl": "https://media.licdn.com/dms/image/C5603AQHgHt3GquPb0g/profile-displayphoto-shrink_400_400/0/1650605489047?e=1696464000&v=beta&t=9056kSSbnv5pNtD6IfTKJ6Facx5LV7myV2FwE5sEv7A",
+          "type": "signedIn",
+          "userId": "123ben",
+          "userSnippylyId": "4658641886513752"
+        },
+        "metadata": {
+          "apiKey": "j3AwoBkuQMTEgeqrmPve",
+          "clientDocumentId": "mixpanel-demo",
+          "documentId": "7528509053768138",
+          "pageInfo": {
+            "baseUrl": "https://mixpanel-demo.vercel.app",
+            "path": "/",
+            "title": "Home",
+            "url": "https://mixpanel-demo.vercel.app/?username=ben"
+          }
+        }
+      })
 
-    let targetComment= ""
 
-    let actionType = ""
-
-    console.log(req.body)
-    res.status(200).send(`Processing: commentAnnotation: ${commentAnnotation}, commentType: ${targetComment}, actionType ${actionType}`)
-  }
+}
   
